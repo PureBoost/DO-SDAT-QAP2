f376b69ab3ffddf60db64104ec9b77cd28ff9c60
PureBoost <lucasparsons@outlook.com>
2026-06-13 11:44:38 -0230

    Inital Commit

---
+++ src/main/java/com/qap2/qap2/Qap2Application.java
@@ -0,0 +1,13 @@
+package com.qap2.qap2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qap2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Qap2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/qap2/qap2/Qap2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qap2.qap2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Qap2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
